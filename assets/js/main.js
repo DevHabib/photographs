1982d73a3307be2fbb073b25affc6b0a78e89703
Md Habibur Rahman <45137768+DevHabib@users.noreply.github.com>
2019-11-02 14:51:53 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,18 @@
 		// Owl carousel
 		$('.owl-theme').owlCarousel({
 			items: 3,
-			loop:true,
-			autoplay:true,
+			loop: true,
+			autoplay: true,
 			margin: 25,
 			dots: true,
 			responsive: {
-				992:{
+				992: {
 					items: 3
 				},
-				768:{
+				768: {
 					items: 2
 				},
-				320:{
+				320: {
 					items: 1
 				}
 			}
@@ -28,63 +28,51 @@
 		$('.image-popup').magnificPopup({
 			type: 'image',
 			gallery: {
-					enabled: true
-				}
-		})
-		$('.video-popup').magnificPopup({
-			type: 'iframe'
-		})
-
-		
+				enabled: true
+			}
+		});
 		// Magnific Popup Ends
+		// Filtering
+		$(".filter-button").click(function () {
+			var value = $(this).attr('data-filter');
 
+			if (value == "all") {
+				$('.filter').show('1000');
+			} else {
+				$(".filter").not('.' + value).hide('3000');
+				$('.filter').filter('.' + value).show('3000');
+
+			}
+		});
+		// Filtering Ends
+
+		// Collapse
+		$('.nav-toggle').click(function () {
+			var collapse_content_selector = $(this).attr('href');
+			var toggle_switch = $(this);
+			$(collapse_content_selector).toggle(function () {
+				if ($(this).css('display') == 'none') {
+					toggle_switch.html('Read More');
+				} else {
+					toggle_switch.html('Read Less');
+				}
+			});
+		});
+		// Collapse ends
+
+		$('#slides').slideshow({
+			randomize: false,      // Randomize the play order of the slides.
+			slideDuration: 6000,  // Duration of each induvidual slide.
+			fadeDuration: 1000,    // Duration of the fading transition. Should be shorter than slideDuration.
+			animate: true,        // Turn css animations on or off.
+			pauseOnTabBlur: true, // Pause the slideshow when the tab is out of focus. This prevents glitches with setTimeout().
+			enableLog: false      // Enable log messages to the console. Useful for debugging.
+		})
 	});
 
-	// Navbar starts
-	jQuery(document).ready(function(){
-	    function resizeNav() {
-	        $(".menu").css({"height": window.innerHeight});
-	        var radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
-	        var diameter = radius * 2;
-	        $(".nav-layer").width(diameter);
-	        $(".nav-layer").height(diameter);
-	        $(".nav-layer").css({"margin-top": -radius, "margin-left": -radius});
-	    }
-	    $(".nav-toggle").click(function() {
-	        $(".nav-toggle, .nav-layer, .menu").toggleClass("open");
-	    });
-	    $(window).resize(resizeNav);
-	    resizeNav();
-	});
-	// navbar ends
 
 
 
 
 }(jQuery));
 
-// Count Down JS
-
-const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
-
-let countDown = new Date('December 20, 2019 03:00:00').getTime(),
-    x = setInterval(function() {
-
-      let now = new Date().getTime(),
-          distance = countDown - now;
-
-      	document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-      
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
-
-    }, second)
